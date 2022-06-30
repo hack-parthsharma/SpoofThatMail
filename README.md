cb7c7c3f61dd1f5319fccac9fcd803b5bcaf7a7c
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2022-06-30 07:53:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# SpoofThatMail
+Bash script to check if a domain or list of domains can be spoofed based in DMARC records
+
+
+File with domains:
+```
+sh SpoofThatMail.sh -f domains.txt
+```
+One single domain:
+```
+sh SpoofThatMail.sh -d domain
+```
+![image](https://user-images.githubusercontent.com/23397910/149308305-67364f17-9c17-45e5-a023-3ce53bce22ba.png)
+
+The script may not work if sp param is before p param (currently working on this)
+
+Test manually using nslookup -type=txt _dmarc.domain.com
---
+++ SpoofThatMail.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+help () {
+	echo -e "Accepted parameters:\n"
+	echo "Use -d along with a domain name, example sh SpoofThatMail.sh -d domain.com"
+	echo -e "Null string will be detected and ignored\n"
+	echo "Use -f along with a file containing domain names, example sh SpoofThatMail.sh -f domains.txt"
+	echo -e "Note that the path provided for the file must be a valid one\n"
+}
+
+check_url () {
+	domain=$1
+
+	retval=0
+	output=$(nslookup -type=txt _dmarc."$domain")
+	case "$output" in
+		*p=reject*)
+			echo "$domain is ${GREEN}NOT vulnerable${NC}"
+		;;
+		*p=quarantine*)
+			echo "$domain ${YELLOW}can be vulnerable${NC} (email will be sent to spam)"
+		;;
+		*p=none*)
+			echo "$domain is ${RED}vulnerable${NC}"
+			retval=1
+		;;
+		*)
+			echo "$domain is ${RED}vulnerable${NC} (No DMARC record found)"
+			retval=1
+		;;
+	esac
+	return $retval
+}
+
+check_file () {
+
+		input=$1
+		
+		COUNTER=0
+		VULNERABLES=0
+		while IFS= read -r line
+		do
+			COUNTER=$((COUNTER=COUNTER+1))
+			check_url $line
+			VULNERABLES=$((VULNERABLES=VULNERABLES+$?))
+		done < $input
+		echo -e "\n$VULNERABLES out of $COUNTER domains are ${RED}vulnerable ${NC}"
+
+}
+
+main () {
+
+	while getopts d:f: flag
+	do
+		case "${flag}" in
+			f) file=${OPTARG};;
+			d) domain=${OPTARG};;
+		esac
+	done
+
+	if [ -n "$domain" ]; then
+		check_url $domain
+	elif [ -f "$file" ]; then
+		check_file $file
+	else
+		help
+	fi
+
+}
+
+echo "
+███████╗██████╗  ██████╗  ██████╗ ███████╗                          
+██╔════╝██╔══██╗██╔═══██╗██╔═══██╗██╔════╝                          
+███████╗██████╔╝██║   ██║██║   ██║█████╗                            
+╚════██║██╔═══╝ ██║   ██║██║   ██║██╔══╝                            
+███████║██║     ╚██████╔╝╚██████╔╝██║                               
+╚══════╝╚═╝      ╚═════╝  ╚═════╝ ╚═╝                               
+                                                                    
+████████╗██╗  ██╗ █████╗ ████████╗    ███╗   ███╗ █████╗ ██╗██╗     
+╚══██╔══╝██║  ██║██╔══██╗╚══██╔══╝    ████╗ ████║██╔══██╗██║██║     
+   ██║   ███████║███████║   ██║       ██╔████╔██║███████║██║██║     
+   ██║   ██╔══██║██╔══██║   ██║       ██║╚██╔╝██║██╔══██║██║██║     
+   ██║   ██║  ██║██║  ██║   ██║       ██║ ╚═╝ ██║██║  ██║██║███████╗
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝ by securihub.com 
+                                                                 
+"
+if [ $# != 2  ];then
+	echo -e "Wrong execution\n"
+	help
+	exit 0
+fi
+
+main $@
